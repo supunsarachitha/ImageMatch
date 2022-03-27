eb42e6a214431e2e0a2fe72236c40fe1500e386f
supunsarachitha <supun.sarachitha@gmail.com>
2022-03-27 01:31:55 -0400

    initial commit

---
+++ ImageMatch/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ImageMatch.Models;
+using ImageMatch.ViewModels;
+
+namespace ImageMatch.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
