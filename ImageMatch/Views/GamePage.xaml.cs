04d68f1d9ad0e3523dce2a1ed54ca6c325400ccb
supun <supun.sarachitha@hotmail.com>
2022-03-30 17:47:39 -0400

    game logic updated

--- ImageMatch/Views/GamePage.xaml.cs
+++ ImageMatch/Views/GamePage.xaml.cs
@@ -9,18 +9,31 @@ namespace ImageMatch.Views
 	public partial class GamePage : ContentPage
 	{
 		public List<SelectedItems> SelectedIcons= new List<SelectedItems>();
+		List<RemoteImageList> remoteImageList = new List<RemoteImageList>();
+		List<SelectedItems> hiddenImageList = new List<SelectedItems>();
+
 
 		public GamePage ()
 		{
 			InitializeComponent ();
 
+			GetRemoteImages();
+
 			int colCount = 6;
 			int rowCount = 6;
+			int SimilarSets = 1;
+
+			var random = new Random();
+
+
+			int hiddenImageListId = 0;
 
 			for (int column = 0; column <= colCount; column++)
             {
                 for (int row = 0; row < rowCount; row++)
                 {
+					
+
 					Button button = new Button()
 					{
 						CornerRadius = 25,
@@ -29,51 +42,109 @@ namespace ImageMatch.Views
 						HeightRequest=50,
 						HorizontalOptions=LayoutOptions.Fill,
 						BorderColor=Color.WhiteSmoke,
-						BorderWidth = 1
+						BorderWidth = 1,
+						ImageSource=""
 					};
                     button.Clicked += Button_Clicked;
-
 					gamegrid.Children.Add(button, column, row); //view , column, row
+
+
+					//creating a map and assign images to a list randomly
+					var nextImageIndex = random.Next(remoteImageList.Count);
+					var nextImage = remoteImageList[nextImageIndex];
+					
+					hiddenImageList.Add(
+					new SelectedItems
+					{
+						ImageUrl = nextImage.URL,
+						Id = hiddenImageListId,
+						GridColumn = column,
+						GridRow = row,
+						IsMatched = false,
+						TypeId = nextImage.Id
+					});
+
+					hiddenImageListId++;
 				}
 			}
 		}
 
+        private void GetRemoteImages()
+        {
+			
+			int i = 0;
+			remoteImageList.Add(new RemoteImageList()
+			{
+				Id = i,
+				URL= "https://www.iconfinder.com/icons/3316547/download/png/128"
+			});
+
+			remoteImageList.Add(new RemoteImageList()
+			{
+				Id = 2,
+				URL = "https://www.iconfinder.com/icons/3316551/download/png/128"
+			});
+
+			remoteImageList.Add(new RemoteImageList()
+			{
+				Id = 3,
+				URL = "https://www.iconfinder.com/icons/3316544/download/png/128"
+			});
+
+		}
+
         private void Button_Clicked(object sender, EventArgs e)
         {
-			//clear un matching images
-			if (SelectedIcons.Count > 1)
-			{
-				var unmatchItem = SelectedIcons.Where(x => x.IsMatched==false).FirstOrDefault();
-				if(unmatchItem != null)
-				{
-					unmatchItem.Button.ImageSource = "";
-				
-						SelectedIcons.Remove(unmatchItem);
-				
-				}
-			}
 
+			////clear un-matching images
+			//if (SelectedIcons.Count > 1)
+			//{
+			//	var unmatchItem = SelectedIcons.Where(x => x.IsMatched==false).FirstOrDefault();
+			//	if(unmatchItem != null)
+			//	{
+			//		unmatchItem.Button.ImageSource = "";
+			//		SelectedIcons.Remove(unmatchItem);
+				
+			//	}
+			//}
+
+			//create an object by selected button and add to a list
 			SelectedItems selected = new SelectedItems();
 			selected.Button = (Button)sender;
 			selected.GridRow = Grid.GetRow(selected.Button);
 			selected.GridColumn = Grid.GetColumn(selected.Button);
-			selected.TypeId = 1;
-			selected.Button.ImageSource = "https://www.iconfinder.com/icons/3316547/download/png/128";
-			int typeId = 1;
+			selected.Id = SelectedIcons.Count() + 1;
 
-			int id = SelectedIcons.Count() + 1;
+			var selectedHiddenItem = hiddenImageList.Where(x => x.GridRow == selected.GridRow && x.GridColumn == selected.GridColumn).FirstOrDefault();
+			
+			selected.TypeId = selectedHiddenItem.TypeId;
+			selected.Button.ImageSource = selectedHiddenItem.ImageUrl;
 
 			if (SelectedIcons.Count > 0)
 			{
-				var lastInItem = SelectedIcons.OrderByDescending(x => x.Id).FirstOrDefault();
-				if(typeId == lastInItem.TypeId)
+				//selected item from list
+				var selectedIcon = SelectedIcons.Where(x => x.GridColumn == selected.GridColumn && x.GridRow == selected.GridRow).FirstOrDefault();
+
+				if(selectedIcon != null)
+				{
+					//clear image from selected item list and 
+					selected.Button.ImageSource = "";
+
+					SelectedIcons.Remove(selectedIcon);
+					return;
+				}
+
+				//previous item
+				var similarItems = SelectedIcons.Where(x => x.TypeId== selected.TypeId);
+
+
+				if (similarItems!=null && similarItems.Count()>0)
                 {
 					selected.IsMatched = true;
 				}
                 else
                 {
 					selected.IsMatched = false;
-					selected.TypeId = 2;
 				}
 			}
             else if (SelectedIcons.Count == 0)
@@ -83,19 +154,32 @@ namespace ImageMatch.Views
 
 			SelectedIcons.Add(selected);
 
-
-
 		}
     }
 
     public class SelectedItems
     {
 		public int Id { get; set; }
+
 		public Button Button { get; set; }
+
 		public bool IsMatched { get; set; }
+
 		public int TypeId { get; set; }
+
 		public int GridRow { get; set; }
+
 		public int GridColumn { get; set; }
+
+		public string ImageUrl { get; set; }
+	}
+
+	public class RemoteImageList
+    {
+		public int Id { get; set; }
+
+		public string URL { get; set; }
+
 	}
 }
 
