7d9a1e02c024be317a50a2c42e3caf346a945f9c
supunsarachitha <supun.sarachitha@gmail.com>
2022-03-29 22:23:42 -0400

    Image click logic

--- ImageMatch/Views/GamePage.xaml.cs
+++ ImageMatch/Views/GamePage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Xamarin.Forms;
 
 namespace ImageMatch.Views
 {	
 	public partial class GamePage : ContentPage
-	{	
+	{
+		public List<SelectedItems> SelectedIcons= new List<SelectedItems>();
+
 		public GamePage ()
 		{
 			InitializeComponent ();
@@ -14,7 +17,6 @@ namespace ImageMatch.Views
 			int colCount = 6;
 			int rowCount = 6;
 
-			
 			for (int column = 0; column <= colCount; column++)
             {
                 for (int row = 0; row < rowCount; row++)
@@ -23,7 +25,6 @@ namespace ImageMatch.Views
 					{
 						CornerRadius = 25,
 						BackgroundColor= Color.Transparent,
-						ImageSource = "https://www.iconfinder.com/icons/3316547/download/png/128",
 						WidthRequest= 50,
 						HeightRequest=50,
 						HorizontalOptions=LayoutOptions.Fill,
@@ -35,17 +36,66 @@ namespace ImageMatch.Views
 					gamegrid.Children.Add(button, column, row); //view , column, row
 				}
 			}
-             
-			
 		}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-			var btn = (Button)sender;
+			//clear un matching images
+			if (SelectedIcons.Count > 1)
+			{
+				var unmatchItem = SelectedIcons.Where(x => x.IsMatched==false).FirstOrDefault();
+				if(unmatchItem != null)
+				{
+					unmatchItem.Button.ImageSource = "";
+				
+						SelectedIcons.Remove(unmatchItem);
+				
+				}
+			}
 
-			int row = Grid.GetRow(btn);
-			int column = Grid.GetColumn(btn);
+			SelectedItems selected = new SelectedItems();
+			selected.Button = (Button)sender;
+			selected.GridRow = Grid.GetRow(selected.Button);
+			selected.GridColumn = Grid.GetColumn(selected.Button);
+			selected.TypeId = 1;
+			selected.Button.ImageSource = "https://www.iconfinder.com/icons/3316547/download/png/128";
+			int typeId = 1;
+
+			int id = SelectedIcons.Count() + 1;
+
+			if (SelectedIcons.Count > 0)
+			{
+				var lastInItem = SelectedIcons.OrderByDescending(x => x.Id).FirstOrDefault();
+				if(typeId == lastInItem.TypeId)
+                {
+					selected.IsMatched = true;
+				}
+                else
+                {
+					selected.IsMatched = false;
+					selected.TypeId = 2;
+				}
+			}
+            else if (SelectedIcons.Count == 0)
+			{
+				selected.IsMatched = true;
+			}
+
+			SelectedIcons.Add(selected);
+
+
+
 		}
     }
+
+    public class SelectedItems
+    {
+		public int Id { get; set; }
+		public Button Button { get; set; }
+		public bool IsMatched { get; set; }
+		public int TypeId { get; set; }
+		public int GridRow { get; set; }
+		public int GridColumn { get; set; }
+	}
 }
 
