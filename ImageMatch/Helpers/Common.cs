421b05082fd94f95fc7d486011ef55c9d6bd0bf3
supunsarachitha <supun.sarachitha@gmail.com>
2022-04-09 03:03:20 -0400

    flip effect and common page

---
+++ ImageMatch/Helpers/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ImageMatch.Helpers
+{
+	public class Common
+	{
+		public static string WinToneURL = "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Yung_Kartz/July_2019/Yung_Kartz_-_02_-_Levels.mp3";
+
+		public static string FailToneURL = "";
+
+		public static int MaxFailAttemptCount = 20;
+
+		public static int GridColumnCount = 5;
+
+		public static int GridRowCount = 8;
+
+	}
+}
+
